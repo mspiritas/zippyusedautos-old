928d46e3129ed1fb81155c7f090fc64136ee6d01
Monica <m_spiritas@mail.fhsu.edu>
2021-03-14 23:51:21 -0500

    Addede JawsDB info

--- model/database.php
+++ model/database.php
@@ -1,11 +1,12 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-    //$password = '';
+    $dsn = 'mysql:host=eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
+    ;dbname=u8ckduy45131xbnf';
+    $username = 'n44smu5vkfdfwrj1';
+    $password = 'gcnquhx9y4dq5i0b';
 
     try {
-        $db = new PDO($dsn, $username);
-        // $db = new PDO($dsn, $username, $password);
+        //$db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error = "Database Error: ";
         $error .= $e->getMessage();
